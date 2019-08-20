f94f0c96afe64a2d626bed79928a89808d51cf67
md.taushif <md.taushif@bisdn.de>
2019-08-20 16:18:19 +0200

    25G cards config setup

---
+++ vxlan_dell100.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# This script creates and configures vxlan on the control plane.
+ 
+set -x
+
+VXLAN_ID=200
+VXLAN_IFACE=vxlan$VXLAN_ID
+LOCAL=172.16.248.198
+REMOTE=172.16.248.83
+DOCKER_BR=br-$(docker network ls -fname=dockercomposecp_1_accel_network -q)
+# Uncomment the below instruction to use control traffic split at the client end.
+#REMOTE=172.16.248.151
+
+ip l del $VXLAN_IFACE
+ip l add $VXLAN_IFACE type vxlan id $VXLAN_ID dstport 4789 local $LOCAL remote $REMOTE
+ip l set $VXLAN_IFACE master $DOCKER_BR
+ip l set $VXLAN_IFACE up
